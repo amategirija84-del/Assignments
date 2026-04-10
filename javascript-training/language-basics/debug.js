a7cfbae738b174052c8ac0b9fd6244e650d3bec1
Girija <amategirija84@gmail.com>
2026-04-10 19:07:54 +0530

    message

---
+++ javascript-training/language-basics/debug.js
@@ -0,0 +1,20 @@
+console.log("Executing Line 1");
+console.log("Executing Line 2");
+console.log("Executing Line 3");
+SumOfNumbers(5,10);
+console.log("Executing Line 5");
+console.log("Executing Line 6");
+console.log("Executing Line 7");
+console.log("Executing Line 8");
+console.log("Executing Line 9");
+console.log("Executing Line 10");
+
+
+
+
+function SumOfNumbers(a,b)
+{
+    c=a+b;
+    console.log(c);
+}
+
